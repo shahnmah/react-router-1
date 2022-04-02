8018e9db94387824cbff475be5d9a4b87be55cb5
shahnmah <shahn.mah@gmail.com>
2022-04-03 02:41:01 +0600

    context shared

--- src/components/Aunty/Aunty.js
+++ src/components/Aunty/Aunty.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RingContext } from '../Grandpa/Grandpa';
 
 const Aunty = ({house}) => {
+    const gift = useContext(RingContext)
     return (
         <div>
             <h4>Aunty</h4>
             <p>House: {house}</p>
+            <p>Gift: {gift}</p>
         </div>
     );
 };
